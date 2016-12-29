cd30e38a2eccf8d5884ed5fd9fcce3a350d3818d
NikValdez <nerdvestor@gmail.com>
2016-12-28 19:24:40 -0800

    1 of 2 tagging and deleting tags

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,20 @@
+class TagsController < ApplicationController
+
+  def index
+    @tags = Tag.all    #.sort_by(&:name)
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+  def destroy 
+  	@tag =Tag.find(params[:id])
+  	@tag.destroy
+
+  	flash.notice = "\"#{@tag.name}\" Deleted!"
+
+  	redirect_to tags_path
+  end
+
+end
+
